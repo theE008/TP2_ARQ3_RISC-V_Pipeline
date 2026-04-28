c044387bc3192be6178ee91fa58bb803cf027fae
theE008 <144809113+theE008@users.noreply.github.com>
2026-04-28 16:46:12 -0300

    Versão com algumas melhorias

--- src/BranchUnit.v
+++ src/BranchUnit.v
@@ -14,7 +14,6 @@ module BranchUnit (
     assign opcode = instruction[6:0];
     wire [31:0] branch_imm;
 
-
     assign branch_imm = {
         {20{instruction[31]}},
         instruction[7],
@@ -24,10 +23,20 @@ module BranchUnit (
     };
 
     always @(*) begin
+        // Valores padrão: branch não tomado, PC segue o fluxo normal (PC + 4)
         branch_taken  = 1'b0;
         branch_target = pc_ex + 32'd4;
 
-        //TODO: Implementar lógica do BEQ aqui!!!
+        // Verifica se a instrução atual é um BEQ
+        if (opcode == BEQ) begin
+            // Compara os operandos rs1 e rs2
+            if (rs1_value == rs2_value) begin
+                // Se forem iguais, o branch é tomado
+                branch_taken  = 1'b1;
+                // O novo endereço é o PC da instrução atual (no estágio EX) + o imediato calculado
+                branch_target = pc_ex + branch_imm;
+            end
+        end
     end
 
 endmodule
